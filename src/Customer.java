286e006a9f7b022284b3d293f813fa8cc631fea8
Aaper <aram.parsegyan@gmail.com>
2016-04-28 11:12:03 +0200

    Revert "Replace Temp with Query"
    This reverts commit c9e97c99f789910fa38971f867de5371e5dfb1d9.

--- src/Customer.java
+++ src/Customer.java
@@ -32,8 +32,8 @@ class Customer {
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
