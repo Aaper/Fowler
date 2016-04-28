d4688a311f8d9ac5a4bbb1ec7055043db679a727
Aaper <aram.parsegyan@gmail.com>
2016-04-28 13:15:32 +0200

    Move Method and Conditional with Polymorphism

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -8,4 +8,9 @@ class NewReleasePrice extends Price {
         return daysRented * 3;
     }
 
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
+
+
 }
